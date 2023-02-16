36274f7fa83f7b0abe5cef9a18a52e8d77325b13
Lizzie DelMonico <edelmonico@smith.edu>
2023-02-15 23:01:56 -0500

    Lizzie

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,58 @@
-class Conversation {
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
+class Conversation {
+  /**
+   * 
+   * Creates a conversation between the user and the computer. 
+   * @param string given by user
+   * @return void
+   * 
+   */
   public static void main(String[] arguments) {
     // You will start the conversation here.
+    String[] responseList = {"Shocking", "I'm surprised.", "Are you ok?", "Oh wow.", "Mhm."};
+    Scanner myObj = new Scanner(System.in);
+    System.out.println("How many rounds?");
+    String num = myObj.nextLine();
+    int rounds = Integer.valueOf(num);
+    System.out.println("Hi! What's your name?");
+    String userName = myObj.nextLine();
+    for (int i = 0; i < rounds; i++){
+      System.out.println("What's on your mind right now " + userName + "?");
+      String s1 = myObj.nextLine();
+      String[] words = s1.split( " ");
+      Random random = new Random();
+      int index = random.nextInt(responseList.length);
+      for (int w = 0; w < words.length; w++) {
+        if (words[w].equals("me")){
+          words[w] = "you";
+        }
+        
+        if (words[w].equals("am")){
+          words[w] = "are";
+        }
+        if (words[w].equals("you")){
+          words[w] = "I";
+        }
+        if (words[w].equals("my")){
+          words[w] = "your";
+        }
+        if (words[w].equals("your")){
+          words[w] = "my";
+        }
+        if (words[w].equals("I")){
+          words[w] = "you";
+        }
+        else{
+          
+
+        }
+       
+      } 
+      System.out.println(String.join(" ", words));
+    }
+    System.out.println("Bye!!");
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+Eliza Gilchrist Tahiya Chowdhury
 - Which session(s) of TA / office hours did you attend?
+Tuesday 2/14 and Wednesday 2/15
 - What are your initial impressions of Java? 
+It has been very challenging to get used to the switch from Python to Java. The formatting is a bit different and tedious.
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+There are some important features in both coding languages such as if statements and lists. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+I had so many questions while working on this assignment. I had to test many different pieces of code only to find out that they didn't work. Luckily, TA hours helped me a lot and I was able to get more done on this assignment than the last one. 
